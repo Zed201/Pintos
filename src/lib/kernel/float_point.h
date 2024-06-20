70e78706ed0fff885b3092c87cc345dd650dd20f
Luiz Gustavo <96800329+Zed201@users.noreply.github.com>
2024-06-19 21:10:05 -0300

    Update float_point.h

--- src/lib/kernel/float_point.h
+++ src/lib/kernel/float_point.h
@@ -1,14 +1,12 @@
 #ifndef FLOAT_POINT
 #define FLOAT_POINT
 
-#define FLOAT_SHIFT_AMOUNT 17
 #define Q 14
-#define F mypow(Q)
+#define F (1 << Q)
 
 // vai retornar apenas 2^p, pois tudo é na base 2
-int mypow(int p);
 typedef int float_type;
-
+// Lembrar que nao funciona INT_FLOAT(59/60), apenas se INT_FLOAT(59.0/60)
 // converter integer to float_type
 #define INT_FLOAT(n) ((float_type)(n * F))
 // soma/sub entre dois float type
@@ -26,10 +24,8 @@ typedef int float_type;
 
 // converte x to integer, arredondando para zero
 // nao sei se esse aqui funciona
-#define FLOAT_INT_ZERO(x) (int) (x >> FLOAT_SHIFT_AMOUNT)
 // talvez trocar por isso, na doc ta assim
-// #define FLOAT_INT_ZERO(x) ((int) (x / F)
+#define FLOAT_INT_ZERO(x) ((int) (x / F)
 // round de float
-#define FLOAT_INT_ROUND(x) (x >= 0 ? ((A + (1 * mypow(Q - 1))) / F)) \
-                                : ((x - (1 * mypow(Q - 1))) / F)
+#define FLOAT_INT_ROUND(x) (x >= 0) ? (x + (F/2)) : (x - (F/2))
 #endif
